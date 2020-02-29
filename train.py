fcf4e07893c811023fa7b05fb9d30a7ef40375b3
beiyan <bai1911@foxmail.com>
2020-02-29 21:35:11 +0800

    add config

--- train.py
+++ train.py
@@ -18,7 +18,7 @@ import tensorboardX
 import shutil
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--config', type=str, default='configs/edges2handbags_folder.yaml', help='Path to the config file.')
+parser.add_argument('--config', type=str, default='configs/demo_anime_folder.yaml', help='Path to the config file.')
 parser.add_argument('--output_path', type=str, default='.', help="outputs path")
 parser.add_argument("--resume", action="store_true")
 parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
